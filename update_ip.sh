ddb4163051e0334666f72eaa552412de93290aca
ndnpibot <ndnpibot@gmail.com>
2016-12-06 17:05:04 -0600

    update addr

--- update_ip.sh
+++ update_ip.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
-
 BASE=/home/pi/report
-
 $BASE/ip_report.sh eth0 > $BASE/current_addr 2>&1
-$BASE/ip_report.sh wlan0 > $BASE/current_addr 2>&1
+$BASE/ip_report.sh wlan0 >> $BASE/current_addr 2>&1
 cd $BASE
 git pull && git add -A && git commit -m 'update addr' && git push
