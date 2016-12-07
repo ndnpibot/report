7d0811d3595d01127695894a8689c207e6ca4b69
ndnpibot <ndnpibot@gmail.com>
2016-12-07 00:08:58 -0600

    update addr

--- update_ip.sh
+++ update_ip.sh
@@ -1,8 +1,6 @@
+#! /bin/bash
 BASE=/home/pi/report
-echo 1 > $BASE/debug
-$BASE/ip_report.sh eth0 > $BASE/current_addr 2>&1
-echo 2 >> $BASE/debug
-$BASE/ip_report.sh wlan0 >> $BASE/current_addr 2>&1
-echo 1 >> $BASE/debug
+echo `ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'` > $BASE/current_addr
+echo `ifconfig wlan0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'` > $BASE/current_addr
 cd $BASE
 git pull && git add -A && git commit -m 'update addr' && git push
